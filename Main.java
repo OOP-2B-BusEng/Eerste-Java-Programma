c583e33e74926f778f2e29cd9c5b903af76fb73a
Frederik Gailly <Frederik.Gailly@ugent.be>
2020-10-16 14:47:11 +0000

    aanpassen yrkdy

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello out there.");
+    System.out.println("Hello out there !");
     System.out.println("I will add two numbers for you");
     System.out.println("Enter two whole numbers on a line");
 
